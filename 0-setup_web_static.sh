5be47a5f5fab461fb246b89192e6286e41236315
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-04-03 14:23:26 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ fi
 mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file for testing
-echo "<html><head><title>Test Page</title></head><body><h1>This is a test page.</h1></body></html>" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 
 # Create symbolic link and ensure it's updated
 rm -rf /data/web_static/current
